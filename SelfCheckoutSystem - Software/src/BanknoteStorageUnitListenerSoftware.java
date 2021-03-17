8e5409957c13a6bf90794b534395227e231fdade
maboz <mabozheng@gmail.com>
2021-03-17 13:41:01 -0600

    completed banknote storage unit listener software

--- SelfCheckoutSystem - Software/src/BanknoteStorageUnitListenerSoftware.java
+++ SelfCheckoutSystem - Software/src/BanknoteStorageUnitListenerSoftware.java
@@ -1,2 +1,43 @@
-public class BanknoteStorageUnitListenerSoftware {
+import org.lsmr.selfcheckout.devices.AbstractDevice;
+import org.lsmr.selfcheckout.devices.BanknoteStorageUnit;
+import org.lsmr.selfcheckout.devices.listeners.AbstractDeviceListener;
+import org.lsmr.selfcheckout.devices.listeners.BanknoteStorageUnitListener;
+
+public class BanknoteStorageUnitListenerSoftware implements BanknoteStorageUnitListener {
+
+    @Override
+    public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+    }
+
+    @Override
+    public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+    }
+
+    @Override
+    public void banknotesFull(BanknoteStorageUnit unit) {
+        if(unit.getCapacity() > 1000)
+        {
+            System.out.println("Banknote Storage Unit is full of banknotes (>1000)");
+        }
+    }
+
+    @Override
+    public void banknoteAdded(BanknoteStorageUnit unit) {
+        System.out.println("Banknote added to Storage Unit");
+
+    }
+
+    @Override
+    public void banknotesLoaded(BanknoteStorageUnit unit) {
+        System.out.println("Banknote loaded to Storage Unit");
+
+    }
+
+    @Override
+    public void banknotesUnloaded(BanknoteStorageUnit unit) {
+        System.out.println("Banknote unloaded from Storage Unit");
+
+    }
 }
--- SelfCheckoutSystem - Software/src/BanknoteValidatorListenerSoftware.java
+++ SelfCheckoutSystem - Software/src/BanknoteValidatorListenerSoftware.java
@@ -1,2 +1,29 @@
-public class BanknoteValidatorListenerSoftware {
+import org.lsmr.selfcheckout.devices.AbstractDevice;
+import org.lsmr.selfcheckout.devices.BanknoteValidator;
+import org.lsmr.selfcheckout.devices.listeners.AbstractDeviceListener;
+import org.lsmr.selfcheckout.devices.listeners.BanknoteValidatorListener;
+
+import java.util.Currency;
+
+public class BanknoteValidatorListenerSoftware implements BanknoteValidatorListener {
+    @Override
+    public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+    }
+
+    @Override
+    public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+    }
+
+    @Override
+    public void validBanknoteDetected(BanknoteValidator validator, Currency currency, int value) {
+        System.out.println("Valid banknote detected - currency: " + currency + "   value: "+ value);
+
+    }
+
+    @Override
+    public void invalidBanknoteDetected(BanknoteValidator validator) {
+        System.out.println("Invalid banknote detected");
+    }
 }
