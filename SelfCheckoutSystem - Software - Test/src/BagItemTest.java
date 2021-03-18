89fb7e8ee8b34a3875edc738e53c1dff5454c150
ManjotKhangura1 <59262828+ManjotKhangura1@users.noreply.github.com>
2021-03-17 22:59:26 -0600

    Merge branch 'main' of https://github.com/Monica-Nguyen/SENG300GroupIteration1 into Jiraiya
     Conflicts:
    	SelfCheckoutSystem - Software/src/ScanItem.java

--- SelfCheckoutSystem - Software - Test/src/BagItemTest.java
+++ SelfCheckoutSystem - Software - Test/src/BagItemTest.java
@@ -1,6 +1,108 @@
 
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.lsmr.selfcheckout.Item;
+import org.lsmr.selfcheckout.devices.OverloadException;
+import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
+import org.lsmr.selfcheckout.devices.SimulationException;
 import org.lsmr.selfcheckoutsoftware.BagItem;
+import org.lsmr.selfcheckoutsoftware.BanknotePayment;
+import org.lsmr.selfcheckoutsoftware.ScanItem;
+
+import java.math.BigDecimal;
+import java.util.Currency;
 
 public class BagItemTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    class Items extends Item
+    {
+        /**
+         * Constructs an item with the indicated weight.
+         *
+         * @param weightInGrams The weight of the item.
+         * @throws SimulationException If the weight is &le;0.
+         */
+        protected Items(double weightInGrams) {
+            super(weightInGrams);
+        }
+    }
+
+    SelfCheckoutStation station;
+    Currency canadianDollars = Currency.getInstance("CAD");
+    BigDecimal nickel = new BigDecimal(0.05);
+    BigDecimal dime = new BigDecimal(.1);
+    BigDecimal quarter = new BigDecimal(.25);
+    BigDecimal loonie = new BigDecimal(1.00);
+    BigDecimal toonie = new BigDecimal(2.00);
+    BigDecimal[] coinDenominations = {nickel, dime, quarter, loonie, toonie};
+    int[] bankNoteDenominations = {5, 10, 20, 50, 100};
+    int scaleMaximumWeight = 100; // Assuming kilograms
+    int scaleSensitivity = 1; // In grams
+
+
+    //BagItem b = new BagItem(station);
+
+    /*public BagItemTest()
+    {
+        SelfCheckoutStation selfCheckoutStation = new SelfCheckoutStation(canadianDollars, bankNoteDenominations,coinDenominations, scaleMaximumWeight, scaleSensitivity);
+        BagItem bagItem = new BagItem(selfCheckoutStation, electListen);
+        Items x = null;
+        bagItem.baggingItem(x);
+    }*/
+
+    @Test
+    public void nullItem()
+    {
+        try
+        {
+            SelfCheckoutStation selfCheckoutStation = new SelfCheckoutStation(canadianDollars, bankNoteDenominations,coinDenominations, scaleMaximumWeight, scaleSensitivity);
+            BagItem bagItem = new BagItem(selfCheckoutStation);
+            Items x = null;
+            bagItem.baggingItem(x);
+        }
+        catch (SimulationException | OverloadException e)
+        {
+            return;
+        }
+        fail("SimulationException expected");
+    }
+
+    @Test
+    public void testBaggingItem() throws OverloadException {
+        SelfCheckoutStation selfCheckoutStation = new SelfCheckoutStation(canadianDollars, bankNoteDenominations,coinDenominations, scaleMaximumWeight, scaleSensitivity);
+        BagItem bagItem = new BagItem(selfCheckoutStation);
+        Item item = new Items(2.34);
+        double itemWeight = item.getWeight();
+        bagItem.baggingItem(item);
+        assertTrue(bagItem.getCheckWeight() == itemWeight);
+    }
+
+    @Test
+    public void testHeavyBaggingItem() {
+        try {
+            SelfCheckoutStation selfCheckoutStation = new SelfCheckoutStation(canadianDollars, bankNoteDenominations, coinDenominations, scaleMaximumWeight, scaleSensitivity);
+            BagItem bagItem = new BagItem(selfCheckoutStation);
+            Item item = new Items(1234.56);
+            bagItem.baggingItem(item);
+        }
+        catch(OverloadException e)
+        {
+            return;
+        }
+        fail("OverloadException expected");
+    }
+
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
 
 }
--- SelfCheckoutSystem - Software/src/org/lsmr/selfcheckoutsoftware/CoinPayment.java
+++ SelfCheckoutSystem - Software/src/org/lsmr/selfcheckoutsoftware/CoinPayment.java
@@ -3,23 +3,132 @@ package org.lsmr.selfcheckoutsoftware;
 
 import org.lsmr.selfcheckout.Coin;
 import org.lsmr.selfcheckout.devices.*;
+import org.lsmr.selfcheckout.devices.listeners.*;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class CoinPayment {
 
 
     private SelfCheckoutStation station;
-    private CoinSlot slot;
-    private Coin coin;
-    private CoinListenerSoftware cls;
 
-    public CoinPayment(SelfCheckoutStation station, CoinListenerSoftware cls) {
+    MathContext m = new MathContext(1); // 4 precision
+    private boolean insertCoin = false;
+    private Coin coin2;
+    private boolean validCoin = false;
+    private boolean addCoin = false;
+    private boolean full = false;
+    BigDecimal totalPaid = BigDecimal.valueOf(0.00).round(m);
+
+    public BigDecimal getTotalPaid()
+    {
+        return totalPaid;
+    }
+
+    public CoinPayment(SelfCheckoutStation station) {
         this.station = station;
-        this.cls = cls;
-        station.coinSlot.register(cls);
-        station.coinValidator.register(cls);
-        station.coinStorage.register(cls);
+        station.coinSlot.register(new CoinSlotListener() {
+
+
+            @Override
+            public void coinInserted(CoinSlot slot) {
+                insertCoin = true;
+                System.out.println("Coin has been inserted");
+            }
+
+            @Override
+            public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+            }
+
+            @Override
+            public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+            }
+        });
+        station.coinValidator.register(new CoinValidatorListener() {
+            @Override
+            public void validCoinDetected(CoinValidator validator, BigDecimal value) {
+                validCoin = true;
+                System.out.println("Coin is valid");
+            }
+
+            @Override
+            public void invalidCoinDetected(CoinValidator validator) {
+                validCoin = false;
+                System.out.println("Coin is invalid");
+
+            }
+
+            @Override
+            public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+            }
+
+            @Override
+            public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+            }
+        });
+        station.coinStorage.register(new CoinStorageUnitListener() {
+            @Override
+            public void coinsFull(CoinStorageUnit unit) {
+                if(unit.getCoinCount()>unit.getCapacity()) {
+                    full = true;
+                    System.out.println("Coin storage is full");
+                }
+            }
+
+            @Override
+            public void coinAdded(CoinStorageUnit unit) {
+                addCoin = true;
+                System.out.println("Coin has been added");
+
+            }
+
+            @Override
+            public void coinsLoaded(CoinStorageUnit unit) {
+            }
+
+            @Override
+            public void coinsUnloaded(CoinStorageUnit unit) {
+
+            }
+
+            @Override
+            public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+            }
+
+            @Override
+            public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+            }
+        });
+        station.coinTray.register(new CoinTrayListener() {
+            @Override
+            public void coinAdded(CoinTray tray) {
+                if (tray.hasSpace())
+                {
+                    System.out.println("Coin added");
+                }
+                else
+                {
+                    System.out.println("Coin not added");
+                }
+            }
+
+            @Override
+            public void enabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+            }
+
+            @Override
+            public void disabled(AbstractDevice<? extends AbstractDeviceListener> device) {
+
+            }
+        });
     }
 
 
@@ -30,44 +139,34 @@ public class CoinPayment {
             throw new SimulationException("Coin cannot be less than or equal to 0");
         }
 
-        try{
+        else {
             station.coinSlot.accept(coin);
-        }catch(DisabledException e){
-            System.out.println("Disable Exception");
+            if(insertCoin){
+                System.out.println("Coin insertion successful");
+                if (!validCoin)
+                {
+                    System.out.println("Coin validation unsuccessful");
+                }
+                else
+                {
+                    System.out.println("Coin is valid");
+                    if (full)
+                    {
+                        System.out.println("Storage is full");
+                    }
+                    else
+                    {
+                        System.out.println("Storage has space");
+                        BigDecimal validatedCoin = coin.getValue().round(m);
+                        totalPaid = totalPaid.add(validatedCoin).round(m);
+                        System.out.println("You paid: " + totalPaid + " dollars");
+                    }
+                }
+            }
+            else {
+                System.out.println("Coin insertion unsuccessful");
+            }
         }
-
-        if(cls.isInsertCoin()){
-            System.out.println("Coin insertion successful");
-        }else{
-            System.out.println("Coin insertion unsuccesssful");
-        }
-
-        try{
-            station.coinValidator.accept(coin);
-        }catch(DisabledException e){
-            System.out.println("Disable Exception");
-        }
-
-        if(cls.isValidCoin()) {
-            System.out.println("Coin validation successful");
-        }else{
-            System.out.println("Coin validation unsuccessful");
-        }
-
-        try{
-            station.coinStorage.accept(coin);
-        }catch(DisabledException e){
-            System.out.println("Disabled Exception");
-        }catch(OverloadException e) {
-            System.out.println("Overload Exception");
-        }
-
-        if(cls.isAddCoin()) {
-            System.out.println("Coin storage successful");
-        }else{
-            System.out.println("Coin storage unsuccessful");
-        }
-
     }
 
 }
